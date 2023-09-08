bf5b67450f1fd366e9aec9e2a8d78bc15e98bc3f
Vinnie-Kim <kimutaiketer033@gmail.com>
2023-09-08 15:10:08 +0300

    Code to generate valid plates in Kenya excluding KDF

---
+++ main.py
@@ -0,0 +1,30 @@
+import time
+
+def generate_valid_plates():
+    valid_plates = []
+
+    for letter1 in range(ord('K'), ord('L')):
+        for letter2 in range(ord('A'), ord('Z') + 1):
+            for letter3 in range(ord('A'), ord('Z') + 1):
+                for number1 in range(10):
+                    for number2 in range(10):
+                        for number3 in range(10):
+                            for letter4 in range(ord('A'), ord('Z') + 1):
+                                plate = f"{chr(letter1)}{chr(letter2)}{chr(letter3)} {number1}{number2}{number3}{chr(letter4)}"
+                                if "000" not in plate and not plate.startswith("KDF"):
+                                    valid_plates.append(plate)
+
+    return valid_plates
+
+start_time = time.time()
+valid_plates = generate_valid_plates()
+end_time = time.time()
+
+total_valid_plates = len(valid_plates)
+
+for plate in valid_plates:
+    print(plate)
+
+execution_time_minutes = (end_time - start_time) / 60
+print(f"Total valid plates: {total_valid_plates}")
+print(f"Total execution time (minutes): {execution_time_minutes:.2f} minutes")
